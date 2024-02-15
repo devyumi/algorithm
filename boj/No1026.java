b1321190980349f25c7ddfaff55c971a15a435e5
devyumi <yyumiwing@naver.com>
2024-02-16 00:29:48 +0900

    2024.02.16.

--- boj/No1026.java
+++ boj/No1026.java
@@ -12,7 +12,6 @@ public class No1026 {
         int n = Integer.parseInt(br.readLine());
         Integer[] a = new Integer[n];
         int[] b = new int[n];
-        int[] tmp = new int[n];
         int sum = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -23,35 +22,16 @@ public class No1026 {
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             b[i] = Integer.parseInt(st.nextToken());
-            tmp[i] = b[i];
         }
 
         Arrays.sort(a, Collections.reverseOrder());
-        Arrays.sort(tmp);
+        Arrays.sort(b);
 
         for (int i = 0; i < n; i++) {
-            int index = binarySearch(tmp, b[i]);
-            sum += a[index] * b[index];
+            sum += a[i] * b[i];
         }
 
         bw.write(String.valueOf(sum));
         bw.close();
-    }
-
-    private static int binarySearch(int[] tmp, int key) {
-        int start = 0;
-        int end = tmp.length - 1;
-
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (tmp[mid] == key) {
-                return mid;
-            } else if (tmp[mid] > key) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        return 0;
     }
 }
