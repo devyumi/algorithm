c12584483ac4eb9c8cf8680a946f3ef8b4b3cc4f
devyumi <yyumiwing@naver.com>
2024-02-19 16:43:28 +0900

    2024.02.19.

--- boj/No1931.java
+++ boj/No1931.java
@@ -1,2 +1,51 @@
-package boj;public class No1931 {
+package boj;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class No1931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Time> time = new ArrayList<>();
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            time.add(new Time(start, end));
+        }
+
+        Collections.sort(time, (o1, o2) -> {
+            if (o1.end == o2.end) {
+                return o1.start - o2.start;
+            }
+            return o1.end - o2.end;
+        });
+
+        int endTime = 0;
+        for (int i = 0; i < n; i++) {
+            if (time.get(i).start >= endTime) {
+                endTime = time.get(i).end;
+                result++;
+            }
+        }
+        
+        bw.write(String.valueOf(result));
+        bw.close();
+    }
+
+    private static class Time {
+        private int start;
+        private int end;
+
+        public Time(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
 }
