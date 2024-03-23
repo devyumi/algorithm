87cc98099b4538fe9bbba1086d733e9f6dd8eb02
devyumi <yyumiwing@naver.com>
2024-03-24 00:22:46 +0900

    2024.03.24.

--- boj/dp/No10844.java
+++ boj/dp/No10844.java
@@ -10,26 +10,26 @@ public class No10844 {
         long[][] dp = new long[n + 1][10];
         long answer = 0;
 
-        for (int i = 0; i < 10; i++) {
-            dp[1][i] = 1;
+        for (int j = 1; j < 10; j++) {
+            dp[1][j] = 1;
         }
 
         for (int i = 2; i < n + 1; i++) {
             dp[i][0] = dp[i - 1][1];
             for (int j = 1; j < 10; j++) {
                 if (j == 9) {
-                    dp[i][9] = dp[i - 1][8] % 1000000000;
+                    dp[i][j] = dp[i - 1][8] % 1000000000;
                 } else {
                     dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
                 }
             }
         }
 
-        for (int i = 1; i < 10; i++) {
-            answer = (answer + dp[n][i]) % 1000000000;
+        for (long j : dp[n]) {
+            answer += j;
         }
 
-        bw.write(String.valueOf(answer));
+        bw.write(String.valueOf(answer % 1000000000));
         bw.close();
     }
 }
