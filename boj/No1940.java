a775f9e6b703a57a488cc5551ea1b82a9dded4aa
devyumi <yyumiwing@naver.com>
2024-04-05 23:50:48 +0900

    2024.04.05.

--- boj/No1940.java
+++ boj/No1940.java
@@ -4,27 +4,27 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-//투포인터
 public class No1940 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        int result = 0;
-        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
+        int answer = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
 
+        Arrays.sort(arr);
         int start = 0;
         int end = n - 1;
 
         while (start < end) {
             if (arr[start] + arr[end] == m) {
-                result++;
+                answer++;
                 start++;
                 end--;
             } else if (arr[start] + arr[end] < m) {
@@ -33,7 +33,7 @@ public class No1940 {
                 end--;
             }
         }
-        bw.write(String.valueOf(result));
+        bw.write(String.valueOf(answer));
         bw.close();
     }
 }
