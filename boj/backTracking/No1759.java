4aadcfb9869c29cef665e882b7a1b09038cf5c42
devyumi <yyumiwing@naver.com>
2024-05-03 22:10:24 +0900

    2024.05.03.

--- boj/backTracking/No1759.java
+++ boj/backTracking/No1759.java
@@ -7,20 +7,21 @@ import java.util.StringTokenizer;
 public class No1759 {
     private static int l;
     private static int c;
-    private static BufferedWriter bw;
     private static Character[] arr;
     private static Character[] password;
     private static boolean[] visited;
+    private static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         l = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
         arr = new Character[c];
         password = new Character[l];
         visited = new boolean[c];
+        sb = new StringBuilder();
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < c; i++) {
@@ -29,24 +30,25 @@ public class No1759 {
 
         Arrays.sort(arr);
         backTracking(0, 0);
+        bw.write(sb.deleteCharAt(sb.length() - 1).toString());
         bw.close();
     }
 
-    private static void backTracking(int depth, int start) throws IOException {
+    private static void backTracking(int depth, int start) {
         if (depth == l) {
             if (checkPassword(password)) {
                 for (Character c : password) {
-                    bw.write(c);
+                    sb.append(c);
                 }
-                bw.newLine();
+                sb.append("\n");
             }
             return;
         }
 
         for (int i = start; i < c; i++) {
             if (!visited[i]) {
-                visited[i] = true;
                 password[depth] = arr[i];
+                visited[i] = true;
                 backTracking(depth + 1, i + 1);
                 visited[i] = false;
             }
@@ -56,7 +58,6 @@ public class No1759 {
     private static boolean checkPassword(Character[] password) {
         int j = 0;
         int m = 0;
-
         for (Character c : password) {
             if (c.equals('a') || c.equals('e') || c.equals('i') || c.equals('o') || c.equals('u')) {
                 m++;
@@ -64,6 +65,7 @@ public class No1759 {
                 j++;
             }
         }
+
         if (m >= 1 && j >= 2) {
             return true;
         } else {
