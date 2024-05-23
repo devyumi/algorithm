b8ae83672b1a1506e5c3a6691736bd3dece35a6e
devyumi <yyumiwing@naver.com>
2024-05-23 23:39:40 +0900

    2024.05.23.

--- boj/backTracking/No14888.java
+++ boj/backTracking/No14888.java
@@ -6,59 +6,54 @@ import java.util.StringTokenizer;
 public class No14888 {
     private static int n;
     private static int[] arr;
-    private static int[] operator;
-    private static int max = -(int) 1e9;
-    private static int min = (int) 1e9;
+    private static int[] operation;
+    private static int max;
+    private static int min;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
         arr = new int[n];
-        operator = new int[4];
+        operation = new int[4];
+        max = -(int) 1e9;
+        min = (int) 1e9;
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
-            operator[i] = Integer.parseInt(st.nextToken());
+            operation[i] = Integer.parseInt(st.nextToken());
         }
-
         backTracking(arr[0], 1);
-        bw.write(String.valueOf(max));
-        bw.write("\n" + min);
+        bw.write(max + "\n" + min);
         bw.close();
     }
 
-    private static void backTracking(int number, int depth) {
+    private static void backTracking(int result, int depth) {
         if (depth == n) {
-            max = Math.max(max, number);
-            min = Math.min(min, number);
+            max = Math.max(max, result);
+            min = Math.min(min, result);
             return;
         }
 
         for (int i = 0; i < 4; i++) {
-            if (operator[i] > 0) {
-                operator[i]--;
+            if (operation[i] > 0) {
+                operation[i]--;
 
-                switch (i) {
-                    case 0:
-                        backTracking(number + arr[depth], depth + 1);
-                        break;
-                    case 1:
-                        backTracking(number - arr[depth], depth + 1);
-                        break;
-                    case 2:
-                        backTracking(number * arr[depth], depth + 1);
-                        break;
-                    case 3:
-                        backTracking(number / arr[depth], depth + 1);
-                        break;
+                if (i == 0) {
+                    backTracking(result + arr[depth], depth + 1);
+                } else if (i == 1) {
+                    backTracking(result - arr[depth], depth + 1);
+                } else if (i == 2) {
+                    backTracking(result * arr[depth], depth + 1);
+                } else if (i == 3) {
+                    backTracking(result / arr[depth], depth + 1);
                 }
-                operator[i]++;
+                operation[i]++;
             }
         }
     }
