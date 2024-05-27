1074956e3adcf17f50a27cf57d6b0a7ddae59688
devyumi <yyumiwing@naver.com>
2024-05-27 18:21:47 +0900

    2024.05.27.

--- boj/dp/No11727.java
+++ boj/dp/No11727.java
@@ -2,23 +2,25 @@ package boj.dp;
 
 import java.io.*;
 
-public class No1463 {
+public class No11727 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         int[] dp = new int[n + 1];
 
-        for (int i = 2; i < n + 1; i++) {
-            dp[i] = dp[i - 1] + 1;
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+        if (n == 1) {
+            bw.write("1");
+        } else if (n == 2) {
+            bw.write("3");
+        } else {
+            dp[1] = 1;
+            dp[2] = 3;
+            for (int i = 3; i < n + 1; i++) {
+                dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
             }
-            if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-            }
+            bw.write(String.valueOf(dp[n]));
         }
-        bw.write(String.valueOf(dp[n]));
         bw.close();
     }
 }
