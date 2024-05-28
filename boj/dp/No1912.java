a1936334bc8b67f9e301a2c423285080c0473d29
devyumi <yyumiwing@naver.com>
2024-05-28 23:14:57 +0900

    2024.05.28.

--- boj/dp/No1912.java
+++ boj/dp/No1912.java
@@ -9,19 +9,15 @@ public class No1912 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
         int[] dp = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+        dp[0] = Integer.parseInt(st.nextToken());
 
-        dp[0] = arr[0];
         for (int i = 1; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            int tmp = Integer.parseInt(st.nextToken());
+            dp[i] = Math.max(dp[i - 1] + tmp, tmp);
         }
-
         bw.write(String.valueOf(Arrays.stream(dp).max().getAsInt()));
         bw.close();
     }
