8929f32ef84e6cbe3a99b1d7ad70ca50dfcd5b18
devyumi <yyumiwing@naver.com>
2024-05-29 17:25:32 +0900

    2024.05.29.

--- boj/dp/No1520.java
+++ boj/dp/No1520.java
@@ -3,8 +3,9 @@ package boj.dp;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class No1937 {
+public class No1520 {
     private static int n;
+    private static int m;
     private static int[][] arr;
     private static int[][] dp;
     private static final int[] dx = {-1, 1, 0, 0};
@@ -13,39 +14,40 @@ public class No1937 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n][n];
-        dp = new int[n][n];
-        int max = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n][m];
+        dp = new int[n][m];
 
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                max = Math.max(max, dfs(i, j));
-            }
-        }
-        bw.write(String.valueOf(max));
+        bw.write(String.valueOf(dfs(0, 0)));
         bw.close();
     }
 
     private static int dfs(int x, int y) {
-        if (dp[x][y] != 0) {
+        if (x == n - 1 && y == m - 1) {
+            return 1;
+        }
+
+        if (dp[x][y] != -1) {
             return dp[x][y];
         }
 
-        dp[x][y] = 1;
+        dp[x][y] = 0;
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[x][y] < arr[nx][ny]) {
-                dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && arr[x][y] > arr[nx][ny]) {
+                dp[x][y] += dfs(nx, ny);
             }
         }
         return dp[x][y];
