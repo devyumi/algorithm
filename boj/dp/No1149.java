dcdf72a18de7719f4a32a7926b8c89fa09d7db26
devyumi <yyumiwing@naver.com>
2024-06-03 23:51:04 +0900

    2024.06.03

--- boj/dp/No1149.java
+++ boj/dp/No1149.java
@@ -1,6 +1,7 @@
 package boj.dp;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class No1149 {
@@ -9,7 +10,7 @@ public class No1149 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         int[][] arr = new int[n][3];
-        int[][] dp = new int[n][3];
+        int[][] dp = new int[n + 1][3];
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,17 +19,12 @@ public class No1149 {
             }
         }
 
-        for (int i = 0; i < 3; i++) {
-            dp[0][i] = arr[0][i];
+        for (int i = 1; i < n + 1; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i - 1][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i - 1][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i - 1][2];
         }
-
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
-        }
-
-        bw.write(String.valueOf(Math.min(Math.min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2])));
+        bw.write(String.valueOf(Arrays.stream(dp[n]).min().getAsInt()));
         bw.close();
     }
 }
