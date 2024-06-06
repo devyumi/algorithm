2bf80b0b3e6a31d08f5fd3bf801e843312db93de
devyumi <yyumiwing@naver.com>
2024-06-06 18:08:23 +0900

    2024.06.06.

--- boj/bfs/No2468.java
+++ boj/bfs/No2468.java
@@ -1,85 +1,61 @@
 package boj.bfs;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class No2468 {
     private static int n;
     private static int[][] arr;
     private static boolean[][] visited;
+    private static final int[] dx = {-1, 1, 0, 0};
+    private static final int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         n = Integer.parseInt(br.readLine());
         arr = new int[n][n];
-        visited = new boolean[n][n];
         int high = 0;
-        int result = 0;
+        int answer = 0;
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
+                high = Math.max(high, arr[i][j]);
             }
         }
 
-        while (true) {
-            int safety = 0;
+        for (int k = 0; k <= high; k++) {
+            visited = new boolean[n][n];
+            int result = 0;
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (!visited[i][j] && arr[i][j] > high) {
-                        safety += bfs(i, j, high);
+                    if (arr[i][j] > k && !visited[i][j]) {
+                        visited[i][j] = true;
+                        dfs(i, j, k);
+                        result++;
                     }
                 }
             }
-            if (safety == 0) {
+            if (result == 0) {
                 break;
             }
-
-            result = Math.max(result, safety);
-            high += 1;
-            visited = new boolean[n][n];
+            answer = Math.max(answer, result);
         }
-        bw.write(String.valueOf(result));
+        bw.write(String.valueOf(answer));
         bw.close();
     }
 
-    private static int bfs(int x, int y, int high) {
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
+    private static void dfs(int x, int y, int high) {
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(x, y));
-        visited[x][y] = true;
-
-        while (!queue.isEmpty()) {
-            Point point = queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = point.x + dx[i];
-                int ny = point.y + dy[i];
-
-                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                    if (!visited[nx][ny] && arr[nx][ny] > high) {
-                        queue.offer(new Point(nx, ny));
-                        visited[nx][ny] = true;
-                    }
-                }
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] > high && !visited[nx][ny]) {
+                visited[nx][ny] = true;
+                dfs(nx, ny, high);
             }
-        }
-        return 1;
-    }
-
-    private static class Point {
-        private int x;
-        private int y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
         }
     }
 }
--- boj/bfs/No5014.java
+++ boj/bfs/No5014.java
@@ -7,56 +7,47 @@ import java.util.StringTokenizer;
 
 public class No5014 {
     private static int f;
-    private static int s;
     private static int g;
     private static int u;
     private static int d;
-    private static int[] arr;
-    private static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         f = Integer.parseInt(st.nextToken());
-        s = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
         g = Integer.parseInt(st.nextToken());
         u = Integer.parseInt(st.nextToken());
         d = Integer.parseInt(st.nextToken());
-        arr = new int[f + 1];
-        visited = new boolean[f + 1];
 
         bw.write(bfs(s));
         bw.close();
     }
 
-    private static String bfs(int x) {
+    private static String bfs(int start) {
+        int[] weight = new int[f + 1];
+        boolean[] visited = new boolean[f + 1];
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(x);
-        arr[x] = 0;
-        visited[x] = true;
+        queue.offer(start);
+        visited[start] = true;
 
         while (!queue.isEmpty()) {
             int now = queue.poll();
-
             if (now == g) {
-                return String.valueOf(arr[now]);
+                return weight[g] + "";
             }
 
-            if (now + u <= f && !visited[now + u]) {
-                if (arr[now + u] == 0) {
-                    arr[now + u] = arr[now] + 1;
-                    visited[now + u] = true;
-                    queue.offer(now + u);
-                }
+            if (now + u >= 1 && now + u <= f && !visited[now + u]) {
+                weight[now + u] = weight[now] + 1;
+                visited[now + u] = true;
+                queue.offer(now + u);
             }
 
-            if (now - d > 0 && !visited[now - d]) {
-                if (arr[now - d] == 0) {
-                    arr[now - d] = arr[now] + 1;
-                    visited[now - d] = true;
-                    queue.offer(now - d);
-                }
+            if (now - d >= 1 && now - d <= f && !visited[now - d]) {
+                weight[now - d] = weight[now] + 1;
+                visited[now - d] = true;
+                queue.offer(now - d);
             }
         }
         return "use the stairs";
