6b8ea9cde8fff6428a9b8ad68f34f97c68834e01
devyumi <yyumiwing@naver.com>
2024-08-02 20:48:48 +0900

    2024.08.02.

--- src/boj/implementation/No23288.java
+++ src/boj/implementation/No23288.java
@@ -3,9 +3,10 @@ package boj.implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-//미완성
 public class No23288 {
 
   private static int n;
@@ -16,7 +17,6 @@ public class No23288 {
   private static int[] dice = {2, 4, 1, 3, 5, 6}; //밑면: index 5
   private static int[] dx = {0, 1, 0, -1};  //동 남 서 북
   private static int[] dy = {1, 0, -1, 0};
-  private static int cnt;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,7 +26,6 @@ public class No23288 {
     int k = Integer.parseInt(st.nextToken());
     arr = new int[n][m];
     score = new int[n][m];
-    visited = new boolean[n][m];
     int answer = 0;
 
     //주사위 초기 Point (0, 0)
@@ -42,21 +41,10 @@ public class No23288 {
       }
     }
 
-    //1. score 계산 (dfs 사용)
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (!visited[i][j]) {
-          cnt = 1;
-          visited[i][j] = true;
-          dfs(i, j);
-        }
-      }
-    }
-
     //이동 횟수만큼 반복
     for (int i = 0; i < k; i++) {
 
-      //2. 주사위 한 칸 구르기. map을 벗어나면 direction 반대로 지정
+      //1. 주사위 한 칸 구르기. map을 벗어나면 direction 반대로 지정
       int nx = dicePoint[0] + dx[direction];
       int ny = dicePoint[1] + dy[direction];
       if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
@@ -71,12 +59,13 @@ public class No23288 {
         }
       }
 
-      //주사위 이동함
+      //2. 주사위 이동함
       dicePoint[0] += dx[direction];
       dicePoint[1] += dy[direction];
 
       //3. 점수 획득
-      answer += score[dicePoint[0]][dicePoint[1]];
+      visited = new boolean[n][m];
+      answer += bfs(dicePoint[0], dicePoint[1]);
 
       //4. 주사위 전개도 이동
       changeDice(dice, direction);
@@ -132,18 +121,31 @@ public class No23288 {
     }
   }
 
-  private static void dfs(int x, int y) {
-    for (int i = 0; i < 4; i++) {
-      int nx = x + dx[i];
-      int ny = y + dy[i];
+  private static int bfs(int x, int y) {
+    Queue<int[]> queue = new LinkedList<>();
+    int cnt = 0;
 
-      if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && arr[nx][ny] == arr[x][y]) {
-        visited[nx][ny] = true;
-        cnt++;
-        dfs(nx, ny);
+    queue.offer(new int[]{x, y});
+
+    while (!queue.isEmpty()) {
+      int[] now = queue.poll();
+
+      for (int i = 0; i < 4; i++) {
+        int nx = now[0] + dx[i];
+        int ny = now[1] + dy[i];
+
+        if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny]
+            && arr[nx][ny] == arr[x][y]) {
+          visited[nx][ny] = true;
+          cnt++;
+          queue.offer(new int[]{nx, ny});
+        }
       }
     }
-    score[x][y] = arr[x][y] * cnt;
-    return;
+
+    if (cnt == 0) {
+      cnt = 1;
+    }
+    return arr[x][y] * cnt;
   }
 }
