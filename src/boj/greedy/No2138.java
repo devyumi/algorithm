86e4ce1edca5b6cf939781708871e4184673d6ff
devyumi <yyumiwing@naver.com>
2024-12-12 16:45:11 +0900

    chore: .gitignore 추가

---
+++ src/boj/greedy/No2138.java
@@ -0,0 +1,13 @@
+package boj.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class No2138 {
+
+    public static void main(String[] args) {
+
+    }
+}
