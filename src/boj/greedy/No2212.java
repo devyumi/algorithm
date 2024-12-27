dd9d88730f4e84b5383792eecffae31096f98022
devyumi <yyumiwing@naver.com>
2024-12-27 21:18:04 +0900

    2024.12.27. 풀이 시간 40분

--- src/boj/greedy/No2212.java
+++ src/boj/greedy/No2212.java
@@ -1,39 +1,47 @@
 package boj.greedy;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class No2212 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         int k = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        int[] sensor = new int[n - 1];
-        int result = 0;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
+        //집중국이 센서 개수보다 적을 때 계산
         if (n <= k) {
-            bw.write("0");
+            System.out.print(0);
         } else {
+            int[] arr = new int[n];
+            int answer = 0;
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
             Arrays.sort(arr);
 
             for (int i = 0; i < n - 1; i++) {
-                sensor[i] = arr[i + 1] - arr[i];
+                arr[i] = arr[i + 1] - arr[i];
             }
 
-            Arrays.sort(sensor);
+            //최대값 1000000 이상 값 저장 (사용하지 않는 값)
+            arr[n - 1] = 1000001;
+
+            //배열 차 저장 후 다시 정렬
+            Arrays.sort(arr);
+
             for (int i = 0; i < n - k; i++) {
-                result += sensor[i];
+                answer += arr[i];
             }
-            bw.write(String.valueOf(result));
+            System.out.print(answer);
         }
-        bw.close();
+        br.close();
     }
 }
