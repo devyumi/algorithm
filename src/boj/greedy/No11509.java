fae778345cab4c118fdf777ed87d57a4cb21bcf8
devyumi <yyumiwing@naver.com>
2025-01-01 23:45:50 +0900

    2025.01.01. 풀이 시간 24분

--- src/boj/greedy/No11509.java
+++ src/boj/greedy/No11509.java
@@ -6,25 +6,27 @@ import java.util.StringTokenizer;
 public class No11509 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
+
+        //풍선 높이만큼 배열 생성
         int[] arr = new int[1000002];
         int answer = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             int h = Integer.parseInt(st.nextToken());
+
+            //현재 높이+1에 사용한 화살이 있으면 그 화살을 제거하고 해당 높이에 화살을 추가한다.
             if (arr[h + 1] != 0) {
-                arr[h + 1] -= 1;
+                arr[h + 1]--;
             }
-            arr[h] += 1;
+            arr[h]++;
         }
 
         for (int i : arr) {
             answer += i;
         }
-
-        bw.write(String.valueOf(answer));
-        bw.close();
+        System.out.print(answer);
+        br.close();
     }
 }
