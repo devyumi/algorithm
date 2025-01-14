885cc010ef5c9db99ecdf6fa2e2a95972cab427f
devyumi <yyumiwing@naver.com>
2025-01-14 23:32:10 +0900

    2025.01.14. 풀이 시간 82분

--- src/boj/bfs/No7569.java
+++ src/boj/bfs/No7569.java
@@ -1,6 +1,8 @@
 package boj.bfs;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -10,14 +12,15 @@ public class No7569 {
     private static int n;
     private static int h;
     private static int[][][] arr;
-    private static Queue<Point> queue;
+
+    //상하좌우위아래
     private static final int[] dx = {0, 0, -1, 1, 0, 0};
     private static final int[] dy = {0, 0, 0, 0, -1, 1};
     private static final int[] dz = {1, -1, 0, 0, 0, 0};
+    private static Queue<int[]> queue;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
@@ -35,40 +38,42 @@ public class No7569 {
                         isRipen = false;
                     }
 
+                    //익었다면 주변 탐색
                     if (arr[i][j][k] == 1) {
-                        queue.offer(new Point(i, j, k));
+                        queue.offer(new int[]{i, j, k});
                     }
                 }
             }
         }
-
         if (isRipen) {
-            bw.write("0");
+            System.out.print(0);
         } else {
             bfs();
-            bw.write(String.valueOf(calculateDay()));
+            System.out.print(solve());
         }
-        bw.close();
+        br.close();
     }
 
+    //익은 상태 정수가 모두 다르므로 visited 사용할 필요 없음
     private static void bfs() {
         while (!queue.isEmpty()) {
-            Point now = queue.poll();
+            int[] now = queue.poll();
 
             for (int i = 0; i < 6; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
-                int nz = now.z + dz[i];
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+                int nz = now[2] + dz[i];
 
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && nz >= 0 && nz < h && arr[nx][ny][nz] == 0) {
-                    arr[nx][ny][nz] = arr[now.x][now.y][now.z] + 1;
-                    queue.offer(new Point(nx, ny, nz));
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= h || arr[nx][ny][nz] != 0) {
+                    continue;
                 }
+                arr[nx][ny][nz] = arr[now[0]][now[1]][now[2]] + 1;
+                queue.offer(new int[]{nx, ny, nz});
             }
         }
     }
 
-    private static int calculateDay() {
+    private static int solve() {
         int max = -1;
         for (int k = 0; k < h; k++) {
             for (int i = 0; i < n; i++) {
@@ -82,17 +87,5 @@ public class No7569 {
             }
         }
         return max - 1;
-    }
-
-    private static class Point {
-        private int x;
-        private int y;
-        private int z;
-
-        public Point(int x, int y, int z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
     }
 }
