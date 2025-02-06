1f6f106225c2d9fbf83d775ba96bb30327a058d1
devyumi <yyumiwing@naver.com>
2025-02-06 18:52:18 +0900

    2025.02.06. 풀이 시간 18분

--- src/boj/greedy/No13305.java
+++ src/boj/greedy/No13305.java
@@ -1,37 +1,38 @@
 package boj.greedy;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class No13305 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int n = Integer.parseInt(br.readLine());
-        int[] distance = new int[n - 1];
-        int[] gas = new int[n - 1];
+        int n = Integer.parseInt(br.readLine()) - 1;
+        int[] oil = new int[n];
+        int[] distance = new int[n];
+        long answer = 0;
+        int min = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n; i++) {
             distance[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n - 1; i++) {
-            gas[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            oil[i] = Integer.parseInt(st.nextToken());
         }
 
-        int min = gas[0];
-        long sum = (long) min * distance[0];
-
-        for (int i = 1; i < n - 1; i++) {
-            if (min > gas[i]) {
-                min = gas[i];
+        answer = (long) oil[0] * distance[0];
+        min = oil[0];
+        for (int i = 1; i < n; i++) {
+            if (min > oil[i]) {
+                min = oil[i];
             }
-            sum += (long) min * distance[i];
+            answer += (long) min * distance[i];
         }
-        bw.write(String.valueOf(sum));
-        bw.close();
+        System.out.print(answer);
+        br.close();
     }
 }
