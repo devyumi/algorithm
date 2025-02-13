2620ed04942bfd5e1962dbdcba68b93fb06675a3
devyumi <yyumiwing@naver.com>
2025-02-13 23:40:18 +0900

    2025.02.13. 풀이 시간 22분

--- src/boj/bfs/No2589.java
+++ src/boj/bfs/No2589.java
@@ -1,31 +1,28 @@
 package boj.bfs;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class No2589 {
     private static int n;
     private static int m;
     private static char[][] arr;
-    private static int[][] distance;
-    private static boolean[][] visited;
-    private static int[] dx = {-1, 1, 0, 0};
-    private static int[] dy = {0, 0, -1, 1};
-    private static int answer;
+    private static final int[] dx = {-1, 1, 0, 0};
+    private static final int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        String[] str = br.readLine().split(" ");
+        n = Integer.parseInt(str[0]);
+        m = Integer.parseInt(str[1]);
         arr = new char[n][m];
-        answer = 0;
+        int max = 0;
 
         for (int i = 0; i < n; i++) {
-            String[] str = br.readLine().split("");
+            str = br.readLine().split("");
             for (int j = 0; j < m; j++) {
                 arr[i][j] = str[j].charAt(0);
             }
@@ -34,50 +31,41 @@ public class No2589 {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (arr[i][j] == 'L') {
-                    answer = Math.max(answer, bfs(i, j));
+                    max = Math.max(max, bfs(i, j));
                 }
             }
         }
-
-        bw.write(String.valueOf(answer));
-        bw.close();
+        System.out.print(max);
+        br.close();
     }
 
-    private static int bfs(int x, int y) {
-        visited = new boolean[n][m];
-        distance = new int[n][m];
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(x, y));
-        visited[x][y] = true;
+    private static int bfs (int x, int y) {
+        int[][] distance = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
         int max = 0;
 
-        while (!queue.isEmpty()) {
-            Point now = queue.poll();
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+        visited[x][y] = true;
+
+        while(!queue.isEmpty()) {
+            int[] now = queue.poll();
+            max = Math.max(max, distance[now[0]][now[1]]);
 
             for (int i = 0; i < 4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
 
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                    if (!visited[nx][ny] && arr[nx][ny] == 'L') {
-                        distance[nx][ny] = distance[now.x][now.y] + 1;
-                        max = Math.max(max, distance[nx][ny]);
-                        visited[nx][ny] = true;
-                        queue.offer(new Point(nx, ny));
-                    }
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || arr[nx][ny] == 'W' || visited[nx][ny]) {
+                    continue;
                 }
+                //최대 거리 계산
+                distance[nx][ny] = distance[now[0]][now[1]] + 1;
+
+                visited[nx][ny] = true;
+                queue.offer(new int[]{nx, ny});
             }
         }
         return max;
-    }
-
-    private static class Point {
-        private int x;
-        private int y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
     }
 }
