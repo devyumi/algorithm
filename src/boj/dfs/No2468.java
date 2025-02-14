ec9ca09ae2183b7b6f474ab409e9b2d0adf866ac
devyumi <yyumiwing@naver.com>
2025-02-14 21:21:47 +0900

    2025.02.14. 풀이 시간 24분

--- src/boj/dfs/No2468.java
+++ src/boj/dfs/No2468.java
@@ -1,6 +1,8 @@
-package boj.bfs;
+package boj.dfs;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class No2468 {
@@ -12,26 +14,27 @@ public class No2468 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         n = Integer.parseInt(br.readLine());
         arr = new int[n][n];
-        int high = 0;
+        int h = 0;
         int answer = 0;
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
-                high = Math.max(high, arr[i][j]);
+                h = Math.max(h, arr[i][j]);
             }
         }
 
-        for (int k = 0; k <= high; k++) {
+        //높이 0 ~ 최대까지 확인
+        for (int k = 0; k <= h; k++) {
             visited = new boolean[n][n];
             int result = 0;
+
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (arr[i][j] > k && !visited[i][j]) {
+                    if ( arr[i][j] > k && !visited[i][j]) {
                         visited[i][j] = true;
                         dfs(i, j, k);
                         result++;
@@ -43,8 +46,8 @@ public class No2468 {
             }
             answer = Math.max(answer, result);
         }
-        bw.write(String.valueOf(answer));
-        bw.close();
+        System.out.print(answer);
+        br.close();
     }
 
     private static void dfs(int x, int y, int high) {
@@ -52,10 +55,12 @@ public class No2468 {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] > high && !visited[nx][ny]) {
-                visited[nx][ny] = true;
-                dfs(nx, ny, high);
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n || visited[nx][ny] || arr[nx][ny] <= high) {
+                continue;
             }
+
+            visited[nx][ny] = true;
+            dfs(nx, ny, high);
         }
     }
 }
